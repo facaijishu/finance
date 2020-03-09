19a5f7b08c244ec5bcc2099b7916d2426e65afd1
RubyZheng0203 <zyc_mine2@163.com>
2020-03-09 18:01:41 +0800

    Edit

--- application/home/common.php
+++ application/home/common.php
@@ -10,4 +10,33 @@
         }
         error_log("[".date('Y-m-d H:i:s')."] ".$message."\r\n", 3, LOG_PATH."/fa.log");
     }
+    
+    
+    function subStrLen($str,$length)
+    {
+        $val = "";
+        $len = mb_strlen($str);
+        if($len>=$length){
+            $val = mb_substr($str,0,$length,'utf-8').'...';
+        }else{
+            $val = $str;
+        }  
+        return $val;
+    }
+    
+    /**
+     * 中文字符串分割
+     * @param 要分割的字符串 $str
+     * @param 截取几位分割 $count
+     * @return 数组
+     */
+    function zh_str_split($str,$count){
+        $leng   = strlen($str)/3;     //中文长度
+        $arr    = array();
+        for ($i=0; $i < $leng; $i+=$count) {
+            $arr[] = mb_substr($str, $i, $count,'utf-8');
+        }
+        return $arr;
+    }
+    
 ?>
