6ff04488d096a6e75820ea59c768c091fecdabef
RubyZheng0203 <zyc_mine2@163.com>
2020-04-22 13:48:35 +0800

    Edit

--- application/home/controller/Chat.php
+++ application/home/controller/Chat.php
@@ -2,33 +2,33 @@
 namespace app\home\controller;
 use think\Request;
 /**
- * 
+ *
  */
 class Chat extends Base
-{   
-
-	//点对点聊天页面展示
-	public function index()
-    {   
-    	//发送方uid;
-    	$from_uid = session('FINANCE_USER.uid');
-    	
-    	//接收方uid;
-    	$to_uid   = input('to_uid')?input('to_uid'):0;
-    	
-    	//获取发送方头像/姓名
-    	$fromHeadUrl  = session('FINANCE_USER.userPhoto');
-    	$fromUserName = session('FINANCE_USER.realName'); 
-
-    	//获取接收方头像/姓名
-    	$user = model('Member')->where(['uid'=>$to_uid])->find();
-    	if ($user) {
-    	   $toHeadUrl = $user['userPhoto'];
-    	   $toUserName = $user['realName'];
-    	} else {
-    	   $toHeadUrl = '';
-    	   $toUserName = '';
-    	}
+{
+    
+    //点对点聊天页面展示
+    public function index()
+    {
+        //发送方uid;
+        $from_uid = session('FINANCE_USER.uid');
+        
+        //接收方uid;
+        $to_uid   = input('to_uid')?input('to_uid'):0;
+        
+        //获取发送方头像/姓名
+        $fromHeadUrl  = session('FINANCE_USER.userPhoto');
+        $fromUserName = session('FINANCE_USER.realName');
+        
+        //获取接收方头像/姓名
+        $user = model('Member')->where(['uid'=>$to_uid])->find();
+        if ($user) {
+            $toHeadUrl = $user['userPhoto'];
+            $toUserName = $user['realName'];
+        } else {
+            $toHeadUrl = '';
+            $toUserName = '';
+        }
         $this->assign('from_uid',$from_uid);
         $this->assign('to_uid',$to_uid);
         $this->assign('fromheadurl',$fromHeadUrl);
@@ -63,7 +63,7 @@ class Chat extends Base
         
         $list = array_reverse($list);
         $this->assign('list' , $list);
-    	return view();
+        return view();
     }
     
     /**
@@ -71,8 +71,8 @@ class Chat extends Base
      */
     public function selectNewInfo(){
         $data   = input();
-        $model = model("chat");
-        $list = $model->getAllById($data['from_uid'] ,$data['to_uid'], 0 , $data['max'],8);
+        $model  = model("chat");
+        $list   = $model->getAllById($data['from_uid'] ,$data['to_uid'], 0 , $data['max'],8);
         if($list){
             $this->result($list, 1, '搜索成功', 'json');
         } else {
@@ -85,8 +85,8 @@ class Chat extends Base
      */
     public function selectOldInfo(){
         $data   = input();
-        $model = model("chat");
-        $list = $model->getAllById($data['from_uid'] ,$data['to_uid'], $data['min'] , 0,8);
+        $model  = model("chat");
+        $list   = $model->getAllById($data['from_uid'] ,$data['to_uid'], $data['min'] , 0,8);
         if($list){
             $this->result($list, 1, '搜索成功', 'json');
         } else {
@@ -104,7 +104,6 @@ class Chat extends Base
         $data['type']           = $param['type'];
         $data['create_time']    = time();
         $data['is_read']        = 0;
-        
         $model = model("Chat");
         $res   = $model->add($data);
         if($res){
@@ -115,6 +114,7 @@ class Chat extends Base
         }
     }
     
+    
     /**
      * 发送咨询项目链接
      */
@@ -122,18 +122,18 @@ class Chat extends Base
         $datas    = input();
         $from_uid = session('FINANCE_USER.uid');
         
-        $info  = model("project")->getProjectInfoById($id);
-        
         $data['from_uid']       = $from_uid;
         $data['to_uid']         = $datas['uid'];
-        $data['content']        = "<a href='".$datas['link']."'>".$datas['link']."</a>";
+        $data['content']        = "我对项目<a href='".$datas['link']."' style='font-size:16px;color:#ff0000;'>".$datas['send_name']."</a>很感兴趣";
         $data['type']           = 1;
         $data['create_time']    = time();
         $data['is_read']        = 0;
+        
+        
         $model = model("Chat");
         $res   = $model->add($data);
+        $data['content']        = "对项目".$datas['send_name']."很感兴趣";
         if($res){
-            $data['content'] = "关于".$info['pro_name']."的咨询";
             $this->sendToWxTemplate($data);
         }
         $this->result('', 1, "发送成功", 'json');
@@ -141,11 +141,11 @@ class Chat extends Base
     
     //消息持久化
     public function saveMessage()
-    {   
+    {
         $data = [];
         $msg  = '';
         $code = 200;
-
+        
         $param = input("post.");
         $data['from_uid']       = $param['from_uid'];
         $data['to_uid']         = $param['to_uid'];
@@ -162,7 +162,7 @@ class Chat extends Base
         }
         echo json_encode(['code'=>$code,'msg'=>$msg]);
     }
-
+    
     //初始化聊天对话内容列表展示
     public function showChat()
     {
@@ -170,35 +170,35 @@ class Chat extends Base
         $to_uid     = input('to_uid')?input('to_uid'):0;
         $page       = input('page')?input('page'):1;
         $num        = 4;
-        $offset     = ($page-1) * $num;  
-        $map        = "( from_uid =". $from_uid." and to_uid = ". $to_uid .") or ( from_uid = ". $to_uid ." and to_uid =". $from_uid." )"; 
+        $offset     = ($page-1) * $num;
+        $map        = "( from_uid =". $from_uid." and to_uid = ". $to_uid .") or ( from_uid = ". $to_uid ." and to_uid =". $from_uid." )";
         $list       = model('Chat')->where($map)
-                             ->order('chat_id desc')
-                             ->limit($offset,$num)
-                             ->select()
-                             ->toArray();
+                                   ->order('chat_id desc')
+                                   ->limit($offset,$num)
+                                   ->select()
+                                   ->toArray();
         if (!empty($list)) {
             //每隔5分钟展示最新的消息发布时间
             foreach ($list as $key => $each_list) {
-              if ($key == 0) {
-                  $list[$key]['is_show_time'] = friend_date(strtotime($each_list['create_time']));
-              } else {
-                  $last_msg = strtotime($list[$key-1]['create_time']);
-                  $new_msg = strtotime($list[$key]['create_time']);
-                  if ($new_msg - $last_msg >= 300) {
-                      $list[$key]['is_show_time'] = friend_date(strtotime($each_list['create_time']));  
-                  } else {
-                      $list[$key]['is_show_time'] = '';
-                  } 
-              }
+                if ($key == 0) {
+                    $list[$key]['is_show_time'] = friend_date(strtotime($each_list['create_time']));
+                } else {
+                    $last_msg = strtotime($list[$key-1]['create_time']);
+                    $new_msg = strtotime($list[$key]['create_time']);
+                    if ($new_msg - $last_msg >= 300) {
+                        $list[$key]['is_show_time'] = friend_date(strtotime($each_list['create_time']));
+                    } else {
+                        $list[$key]['is_show_time'] = '';
+                    }
+                }
             }
-        } 
+        }
         echo json_encode(['data' => $list]);
     }
     
     //聊天图片上传,返回图片地址
     public function upload()
-    {   
+    {
         
         $file       = $_FILES['file'];
         $from_uid   = input('from_uid')?input('from_uid'):0;
@@ -242,14 +242,14 @@ class Chat extends Base
                 } else {
                     $this->result('' ,0 , '图片来源不合法' , 'json');
                 }
-            } 
-        } 
-
+            }
+        }
+        
     }
-
+    
     //聊天消息列表页
     public function lists()
-    {   
+    {
         $from_uid = session('FINANCE_USER.uid');
         $this->assign('from_uid',$from_uid);
         $this->assign('title' , '');
@@ -261,7 +261,7 @@ class Chat extends Base
         $this->assign('usertype',$userFollow['userType']);
         return view();
     }
-
+    
     //获取当前登录用户未读聊天消息列表页数据
     public function getList()
     {
@@ -273,14 +273,14 @@ class Chat extends Base
     
     //更新未读消息为已读
     public function changeNoread()
-    {   
+    {
         $data = [];
         $from_uid   = input('from_uid')?input('from_uid'):0;
         $to_uid     = input('to_uid')?input('to_uid'):0;
         try {
-          model('Chat')->where(['from_uid'=>$to_uid,'to_uid'=>$from_uid])->update(['is_read'=>1]);
+            model('Chat')->where(['from_uid'=>$to_uid,'to_uid'=>$from_uid])->update(['is_read'=>1]);
             $code = 200;
-            $msg  = 'success';     
+            $msg  = 'success';
         } catch (Exception $e) {
             $code = 201;
             $msg  = $e->getMessage();
@@ -301,28 +301,28 @@ class Chat extends Base
     
     //发送到微信服务器,通知微信用户
     public function sendToWxTemplate($data)
-    {   
+    {
         
         //template_id
         $template_id    = config('chat_content');
-
+        
         $from_uid       = $data['from_uid'];
         $realName       = model('Member')->getRealName($from_uid);
-
+        
         //to_user
         $to_uid         = $data['to_uid'];
         $openid         = model('Member')->getOpenid($to_uid);
         
         //url
         $url            = $_SERVER['SERVER_NAME'].'/home/chat/lists';
-    
+        
         $content        = $data['content'];
         $array = [
-                'first'     => ['value' => '您有一条新的消息' , 'color' => '#173177'],
-                'keyword1'  => ['value' => $realName , 'color' => '#0D0D0D'],
-                'keyword2'  => ['value' => $content , 'color' => '#0D0D0D'],
-                'remark'    => ['value' => '请点击详情查看!' , 'color' => '#173177'],
-            ];
+            'first'     => ['value' => '您有一条新的消息' , 'color' => '#173177'],
+            'keyword1'  => ['value' => $realName , 'color' => '#0D0D0D'],
+            'keyword2'  => ['value' => $content , 'color' => '#0D0D0D'],
+            'remark'    => ['value' => '请点击详情查看!' , 'color' => '#173177'],
+        ];
         $re = sendTemplateMessage($template_id,$openid,$url,$array);
     }
 }
