26333115424216232ca22eae99f4bea9f5f44ec3
RubyZheng0203 <zyc_mine2@163.com>
2020-06-17 10:41:07 +0800

    Edit

--- application/console/controller/Intention.php
+++ application/console/controller/Intention.php
@@ -5,18 +5,22 @@ class Intention extends ConsoleBase{
     public function index(){
         return view();
     }
+    
     public function zryxes(){
         return view();
     }
+    
+    /**
+     * 定增意向列表
+     */
     public function zfmxesread(){
-        $return = [];
-        $order = $this->request->get('order/a', []);
-        $start = $this->request->get('start', 0);
-        $length = $this->request->get('length', config('paginate.list_rows'));
-
-        $create_date1 = $this->request->get('create_date1', '');
-        $create_date2 = $this->request->get('create_date2', '');
-        $neeq = $this->request->get('neeq', '');
+        $return         = [];
+        $order          = $this->request->get('order/a', []);
+        $start          = $this->request->get('start', 0);
+        $length         = $this->request->get('length', config('paginate.list_rows'));
+        $create_date1   = $this->request->get('create_date1', '');
+        $create_date2   = $this->request->get('create_date2', '');
+        $neeq           = $this->request->get('neeq', '');
 
         $zfmxes_intention = model('ZfmxesIntention');
         //搜索条件
@@ -40,31 +44,35 @@ class Intention extends ConsoleBase{
             $zfmxes_intention->order($_order);
         }
         $zfmxes_intention->field('SQL_CALC_FOUND_ROWS *');
-        $list = $zfmxes_intention->limit($start, $length)->select();
+        $list   = $zfmxes_intention->limit($start, $length)->select();
         $result = $zfmxes_intention->query('SELECT FOUND_ROWS() as count');
-        $total = $result[0]['count'];
-        $data = [];
-        $model = model("Member");
+        $total  = $result[0]['count'];
+        $data   = [];
+        $model  = model("Member");
         foreach ($list as $key => $item) {
             $info = $model->getMemberInfoById($item['create_uid']);
-            $list[$key]['create_uid'] = $info['userName'];
+            $list[$key]['userName'] = $info['userName'];
             $data[] = $item->toArray();
         }
-        $return['data'] = $data;
-        $return['recordsFiltered'] = $total;
-        $return['recordsTotal'] = $total;
+        $return['data']             = $data;
+        $return['recordsFiltered']  = $total;
+        $return['recordsTotal']     = $total;
 
         echo json_encode($return);
     }
+    
+    /**
+     * 大宗意向列表
+     */
     public function zryxesread(){
-        $return = [];
-        $order = $this->request->get('order/a', []);
-        $start = $this->request->get('start', 0);
-        $length = $this->request->get('length', config('paginate.list_rows'));
-
-        $create_date1 = $this->request->get('create_date1', '');
-        $create_date2 = $this->request->get('create_date2', '');
-        $neeq = $this->request->get('neeq', '');
+        $return         = [];
+        
+        $order          = $this->request->get('order/a', []);
+        $start          = $this->request->get('start', 0);
+        $length         = $this->request->get('length', config('paginate.list_rows'));
+        $create_date1   = $this->request->get('create_date1', '');
+        $create_date2   = $this->request->get('create_date2', '');
+        $neeq           = $this->request->get('neeq', '');
 
         $zryxes_intention = model('ZryxesIntention');
         //搜索条件
@@ -88,38 +96,54 @@ class Intention extends ConsoleBase{
             $zryxes_intention->order($_order);
         }
         $zryxes_intention->field('SQL_CALC_FOUND_ROWS *');
-        $list = $zryxes_intention->limit($start, $length)->select();
+        $list   = $zryxes_intention->limit($start, $length)->select();
         $result = $zryxes_intention->query('SELECT FOUND_ROWS() as count');
-        $total = $result[0]['count'];
-        $data = [];
-        $model = model("Member");
+        $total  = $result[0]['count'];
+        $data   = [];
+        $model  = model("Member");
         foreach ($list as $key => $item) {
             $info = $model->getMemberInfoById($item['create_uid']);
-            $list[$key]['create_uid'] = $info['userName'];
+            $list[$key]['userName'] = $info['userName'];
             $data[] = $item->toArray();
         }
-        $return['data'] = $data;
-        $return['recordsFiltered'] = $total;
-        $return['recordsTotal'] = $total;
+        $return['data']             = $data;
+        $return['recordsFiltered']  = $total;
+        $return['recordsTotal']     = $total;
 
         echo json_encode($return);
     }
+    
+    
+    /**
+     * 定增意向详细
+     */
     public function detail_zfmxes(){
-        $id = $this->request->param('id/d');
-        $model = model("ZfmxesIntention");
-        $info = $model->getZfmxesIntentionInfoById($id);
-        $model = model("CaijiRemark");
+        $id     = $this->request->param('id/d');
+        
+        $model  = model("ZfmxesIntention");
+        $info   = $model->getZfmxesIntentionInfoById($id);
+        
+        $model  = model("CaijiRemark");
         $remark = $model->getRemarkByCodeDateAndType($info['neeq'] , $info['plannoticeddate'] , 'Intention' , 0);
+        
         $info['remark'] = $remark;
         $this->assign("info" , $info);
         return view();
     }
+    
+    
+    /**
+     * 大宗意向详细
+     */
     public function detail_zryxes(){
-        $id = $this->request->param('id/d');
-        $model = model("ZryxesIntention");
-        $info = $model->getZryxesIntentionInfoById($id);
-        $model = model("CaijiRemark");
+        $id     = $this->request->param('id/d');
+        
+        $model  = model("ZryxesIntention");
+        $info   = $model->getZryxesIntentionInfoById($id);
+        
+        $model  = model("CaijiRemark");
         $remark = $model->getRemarkByCodeDateAndType($info['neeq'] , '' , 'Intention' , $info['id']);
+        
         $info['remark'] = $remark;
         $this->assign("info" , $info);
         return view();
